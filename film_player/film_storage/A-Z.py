98597ee9727da6316e79cde88a19768b4bd62f80
Serhii Sydorenko <sergey.sidorenko09@gmail.com>
2023-02-17 12:45:10 +0200

    Task 13 - JSON

---
+++ film_player/film_storage/A-Z.py
@@ -0,0 +1,5 @@
+import os
+import string
+os.chdir("D:/Users/dn300990ssk/PycharmProjects/hillel_git/film_player/film_storage")
+for c in string.ascii_uppercase:
+    os.makedirs(c)
--- film_player/films_worker.py
+++ film_player/films_worker.py
@@ -1,35 +1,38 @@
-import os
-import requests
-import json
+class Film():
+    def __init__(self, name, rating, year, directed, starring):
+        self.name = name
+        self.rating = rating
+        self.year = year
+        self.directed = directed
+        self.starring = starring
+        self.storage_address = 'адрес'
+    def film_info(self):
+        info = ("Film name is: " + self.name + ". Current rating is: " + self.rating + ". Directed on: " + self.directed + " in " + str(self.year) + ". Starring: " + self.starring).title()
+        print(info)
+    def upload_file(self):
+        import os
+        import string
 
-url = "https://movie-database-alternative.p.rapidapi.com/"
-querystring = {"s": "Avengers Endgame", "r": "json", "page": "1"}
-headers = {
-    "X-RapidAPI-Key": "390fed045bmsh823ad6022efcebap15957cjsn8118bcdd4b76",
-    "X-RapidAPI-Host": "movie-database-alternative.p.rapidapi.com"
-}
-
-BASE_PATH = "film_player/film_storage/"
-
-
-class Film:
-    def __init__(self, film_name):
-        querystring["s"] = film_name
-        response = requests.request("GET", url, headers=headers, params=querystring)
-        content = response.content
-        results = json.loads(content)
-        film = results["Search"][0]
-        self.title = film["Title"]
-        self.year = film["Year"]
-        self.type = film["Type"]
-        self.imdb = "https://www.imdb.com/title/" + film["imdbID"] + "/"
-        self.poster = film["Poster"]
-        self.storage = BASE_PATH + self.title[0] + "/" + self.title + ".txt"
+        for c in string.ascii_uppercase:
+            os.chdir("D:/Users/dn300990ssk/PycharmProjects/hillel_git/film_player/film_storage")
+            if c == self.name[0]:
+                os.chdir("D:/Users/dn300990ssk/PycharmProjects/hillel_git/film_player/film_storage"+str(c))
+                file = open(self.name+'.txt','w')
 
     def get_film_address(self):
-        return self.storage
+        import os
+        address = os.path.abspath(self.name+'.txt')
+        self.storage_address = address
+        print(self.storage_address)
 
-    def upload_file(self):
-        os.chdir(self.title[0])
-        with open(self.title + ".txt", "w") as file:
-            pass
+
+my_favorite_film = Film("Titanic", "7.8 / 10", 1997, "James Cameron", "Leonardo DiCaprio, Kate Winslet, and Billy Zane")
+
+#информация
+my_film.film_info()
+
+#upload_file - створює txt файл зназвою фільму
+my_film.upload_file()
+
+#get_film_address повертає повний шлях(hzljr) до фільму у дериктроії.
+my_film.get_film_address()
