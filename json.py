b4ecc99f2288a0acb8dd7883def4dbeff212e521
Serhii Sydorenko <sergey.sidorenko09@gmail.com>
2023-02-20 20:45:23 +0200

    Task 13 - JSON

--- json.py
+++
@@ -1,8 +0,0 @@
-from script_homework_prew import films_awards
-import json
-from pprint import pprint
-
-films_awards_json = json.dump(films_awards, hsin_file)
-os.chdir("D:/Users/dn300990ssk/PycharmProjects/hillel_git/")
-json_file = open('films_awards_json.json', 'w')
-json_file.write(films_awards_json)
